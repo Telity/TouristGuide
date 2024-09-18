8237ba4b95e52c1c2213f836ab6de82e94b8128c
BjerregaardGG <oliverbjerregaard1@gmail.com>
2024-09-18 10:28:18 +0200

    commit classes

--- src/main/java/gruppe7/turistguide2/Controller/TouristController.java
+++ src/main/java/gruppe7/turistguide2/Controller/TouristController.java
@@ -1,7 +1,59 @@
 package gruppe7.turistguide2.Controller;
 
+import gruppe7.turistguide2.Model.Tourist;
+import gruppe7.turistguide2.Service.TouristService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @Controller
 public class TouristController {
+
+    public final TouristService touristService;
+
+    public TouristController(TouristService touristService) {
+        this.touristService = touristService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Tourist>> getAllAttractions() {
+        List<Tourist> attractions = touristService.getAllAttractions();
+        return new ResponseEntity<>(attractions, HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Tourist> getAttraction(@PathVariable String name) {
+        Tourist attraction = touristService.getAttractionbyName(name);
+        return new ResponseEntity<>(attraction, HttpStatus.OK);
+    }
+
+    @GetMapping("{name}/tags")
+
+
+    @PostMapping("/add")
+    public ResponseEntity<Tourist> addAttraction(@RequestBody Tourist attraction) {
+        Tourist  attraction1 = touristService.addAttraction(attraction);
+        return new ResponseEntity<>(attraction1, HttpStatus.CREATED);
+
+    @PostMapping("/save")
+
+    @PostMapping("/update")
+
+
+    }
+    @PutMapping("/{name}/edit")
+    public ResponseEntity<Tourist> updateAttraction(@PathVariable String name, @RequestBody Tourist attraction) { // uden spørgsmålstegn
+        Tourist attraction2 = touristService.updateAttraction(attraction);
+        return new ResponseEntity<>(attraction2, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{name}")
+    public ResponseEntity<Tourist> deleteAttraction(@RequestBody Tourist attraction) { // med spørgsmålstegn
+        Tourist attraction3 = touristService.deleteAttraction(attraction);
+        return new ResponseEntity<>(attraction3, HttpStatus.OK);
+    }
 
 }
--- src/main/java/gruppe7/turistguide2/Service/TouristService.java
+++ src/main/java/gruppe7/turistguide2/Service/TouristService.java
@@ -1,4 +1,36 @@
 package gruppe7.turistguide2.Service;
 
+import gruppe7.turistguide2.Model.Tourist;
+import gruppe7.turistguide2.Repository.TouristRepository;
+
+import java.util.List;
+
 public class TouristService {
+
+    public final TouristRepository touristRepository;
+
+    public TouristService(TouristRepository touristRepository) {
+        this.touristRepository = touristRepository;
+    }
+
+    public List<Tourist> getAllAttractions() {
+        return touristRepository.getAllAttractions();
+    }
+
+    public Tourist getAttractionbyName(String name) {
+        return touristRepository.getAttractionByName(name);
+    }
+
+    public Tourist addAttraction(Tourist attraction) {
+        return touristRepository.addAttraction(attraction);
+    }
+
+    public Tourist updateAttraction(Tourist attraction) {
+        return touristRepository.updateAttraction(attraction);
+    }
+
+    public Tourist deleteAttraction(Tourist attraction) {
+        return touristRepository.deleteAttraction(attraction);
+    }
+
 }
