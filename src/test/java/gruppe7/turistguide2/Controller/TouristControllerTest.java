4991b9a6e92bfcf8daeb4182f136dba8e2f021e8
nikolajpirum <nikolajpirum.10@gmail.com>
2024-09-26 10:45:23 +0200

    changed java 22 to 21

---
+++ src/test/java/gruppe7/turistguide2/Controller/TouristControllerTest.java
@@ -0,0 +1,50 @@
+package gruppe7.turistguide2.Controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TouristControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllAttractions() {
+    }
+
+    @Test
+    void getAttraction() {
+    }
+
+    @Test
+    void getTag() {
+    }
+
+    @Test
+    void addAttraction() {
+    }
+
+    @Test
+    void saveAttraction() {
+    }
+
+    @Test
+    void deleteAttraction() {
+    }
+
+    @Test
+    void showUpdateForm() {
+    }
+
+    @Test
+    void updateAttraction() {
+    }
+}
